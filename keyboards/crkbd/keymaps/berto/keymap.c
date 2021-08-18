b2eb6afc8331f02c6967dd9b0855cc1d461d4961
Roberto Aguilar <r@rreboto.com>
2021-08-17 22:10:06 -0400

    Add home, end, page up/down to layer

--- keyboards/crkbd/keymaps/berto/keymap.c
+++ keyboards/crkbd/keymaps/berto/keymap.c
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,    KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_GRV,   XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,  KC_F12,
+      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP,                      KC_GRV,   XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_BSLS, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,
+      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_END, KC_PGDN,                      XXXXXXX, KC_BSLS, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL,   KC_LGUI,  KC_LSFT,     KC_DEL, KC_TRNS, XXXXXXX
                                       //`--------------------------'  `--------------------------'
