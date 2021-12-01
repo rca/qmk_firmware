4c6b8e84a2040e9c734c47c7b446d278256cd603
Roberto Aguilar <r@rreboto.com>
2021-12-01 12:09:07 -0500

    Move symbols to left home row

--- keyboards/crkbd/keymaps/berto/keymap.c
+++ keyboards/crkbd/keymaps/berto/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_COLON, KC_MINS,
+      KC_GRV, KC_BSLS, KC_MINS, KC_LBRC, KC_RBRC, KC_PGUP,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_COLON, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_EQL, KC_BSLS, KC_MINS, KC_DOT, KC_LBRC, KC_RBRC,
+      KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN,                      KC_EQL,  KC_BSLS, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, KC_LGUI, KC_LSFT,    KC_TRNS,   KC_SPC,   MO(4)
                                       //`--------------------------'  `--------------------------'
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,    KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP,                      KC_GRV,   XXXXXXX, KC_MINS, XXXXXXX, XXXXXXX,  KC_F12,
+      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME,                      XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_END, KC_PGDN,                      KC_LEAD, KC_BSLS, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC,
+      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END,                      KC_LEAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, KC_LGUI, KC_LSFT,    KC_LEAD,  KC_DEL, XXXXXXX
                                       //`--------------------------'  `--------------------------'
