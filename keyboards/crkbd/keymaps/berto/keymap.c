2a3e6d55ba15ab3743fd6560b6fc38b682aeff68
Roberto Aguilar <r@rreboto.com>
2023-09-27 10:52:21 -0400

    Add insert key and delete key
    On a Windows machine shift+insert is a way to paste into a git bash shell.

--- keyboards/crkbd/keymaps/berto/keymap.c
+++ keyboards/crkbd/keymaps/berto/keymap.c
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,    KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME,                      XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F12,
+      KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX,  KC_INS, KC_HOME,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END,                      KC_LEAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL, KC_END,                       KC_LEAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL, KC_LGUI, KC_LSFT,    KC_LEAD,  KC_DEL, XXXXXXX
                                       //`--------------------------'  `--------------------------'
